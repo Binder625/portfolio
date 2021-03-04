e8b8ca59f7486fb695404ef4f577113b7700cc27
Binder625 <78931433+Binder625@users.noreply.github.com>
2021-03-04 15:32:38 -0500

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,10 @@
+
+let firstname = 'sarah';
+
+console.log(firstname);
+
+function welcome() {
+    let username = prompt("What is your name?");    
+    let message = "Welcome, " + username;
+    document.getElementById('saywelcome').innerHTML = message; 
+}
